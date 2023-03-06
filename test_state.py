c1b5686a552b0ab10ae80752c2d080224bbac3b6
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-03-06 16:20:16 -0500

    gros ménage

--- test_state.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from state import State
-
-def main(self):
-    state = State()
-    self.assertFalse(state.is_valid())
-
-if __name__ == '__main__':
-    unittest.main()
--- test_transition.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from transition import Transition
-
-def main(self):
-    transition = Transition()
-    self.assertFalse(transition.is_valid())
-
-if __name__ == '__main__':
-    unittest.main()
