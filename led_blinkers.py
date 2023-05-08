fb018571ad693aa0d6d6f5ae6a063ef26231e5f8
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2023-05-08 17:03:21 -0400

    merci NG

--- led_blinkers.py
+++ led_blinkers.py
@@ -1,7 +1,10 @@
 from side_blinkers import SideBlinkers
+from Blinker import Blinker
+from robot import Robot
 
 class LedBlinkers(SideBlinkers):
-    def __init__(self, robot) -> None:
+    def __init__(self, left_off_state_generator: Blinker.StateGenerator, left_on_state_generator: Blinker.StateGenerator, right_off_state_generator: Blinker.StateGenerator, right_on_state_generator: Blinker.StateGenerator, robot: Robot) -> None:
+        super.__init__(left_off_state_generator, left_on_state_generator, right_off_state_generator, right_on_state_generator)
         self.robot = robot
     
     
